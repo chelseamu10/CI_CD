f531b3bbf0ae6318b341011b8b3a72f886377f95
Duong <nguyenvanduong01091999@gmail.com>
2021-11-19 18:03:02 +0700

    delete

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import logo from './logo.svg';
-import './App.css';
 import Amplify, { Auth} from 'aws-amplify';
 import { withAuthenticator } from 'aws-amplify-react';
 import awsconfig from './aws-exports'
@@ -9,19 +8,8 @@ Amplify.configure(awsconfig);
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header style={{background:'green',textAlign:'center'}}>
+        <h1 style={{fontWeight:'bold'}}>hello</h1>
       </header>
     </div>
   );
